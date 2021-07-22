2f955ef821a3061ad924911dbf7e7983c63d10b5
john li <jli@circularly.org>
2021-07-22 18:57:02 -0400

    tweak

--- rebuild_and_push_with_container_driver.sh
+++ rebuild_and_push_with_container_driver.sh
@@ -6,13 +6,13 @@ DOCKER_BUILDKIT=1
 docker system prune -a -f
 
 # using docker-container driver per https://github.com/moby/buildkit/issues/1981#issuecomment-785534131
-docker buildx create --driver docker-container --name cache-fail-workaround
-docker buildx build --builder cache-fail-workaround --load \
+docker buildx create --driver docker-container --name cache-bug-workaround
+docker buildx build --builder cache-bug-workaround --load \
     -t circularly/docker-cache-issue-20210722:cachebug-containerdriver \
     --cache-from circularly/docker-cache-issue-20210722:cachebug-containerdriver \
     --build-arg BUILDKIT_INLINE_CACHE=1 \
     .
-docker buildx rm --builder cache-fail-workaround
+docker buildx rm --builder cache-bug-workaround
 docker push circularly/docker-cache-issue-20210722:cachebug-containerdriver
 
 # this causes a change in the local files to simulate a code-only change
