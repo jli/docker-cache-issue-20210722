7d421539a5c7142b560ad31089f20563f1989b40
john li <jli@circularly.org>
2021-08-04 21:39:29 -0400

    docker pull before hand seems to help

---
+++ rebuild_and_push_with_pull.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euo pipefail
+
+DOCKER_BUILDKIT=1
+docker system prune -a -f
+
+# This seems to help!
+docker pull circularly/docker-cache-issue-20210722:cachebug
+
+docker build \
+    -t circularly/docker-cache-issue-20210722:cachebug \
+    --cache-from circularly/docker-cache-issue-20210722:cachebug \
+    --build-arg BUILDKIT_INLINE_CACHE=1 \
+    .
+
+docker push circularly/docker-cache-issue-20210722:cachebug
+
+# this causes a change in the local files to simulate a code-only change
+date > date_log.txt
